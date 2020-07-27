deed61e067c897492829dea50c737d21fb1cb93e
Dmitriy <danny88100@gmail.com>
2020-07-27 19:53:29 -0400

    Updates

---
+++ precache-manifest.e5d6f163d283288f78cdedd58211f5e1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c80beacb9c5eaea19da7d4f9547ab38",
+    "url": "/allamericanbarbershop/index.html"
+  },
+  {
+    "revision": "1c2e12db506cfacdd371",
+    "url": "/allamericanbarbershop/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d822cd63241f116110f7",
+    "url": "/allamericanbarbershop/static/css/main.fbfad1f5.chunk.css"
+  },
+  {
+    "revision": "1c2e12db506cfacdd371",
+    "url": "/allamericanbarbershop/static/js/2.3b37e0ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/allamericanbarbershop/static/js/2.3b37e0ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d822cd63241f116110f7",
+    "url": "/allamericanbarbershop/static/js/main.f7f2caf3.chunk.js"
+  },
+  {
+    "revision": "e708542acb24df3c948a",
+    "url": "/allamericanbarbershop/static/js/runtime-main.7ca277e3.js"
+  },
+  {
+    "revision": "5d6ab5874aacbc4c77277880e1453ad1",
+    "url": "/allamericanbarbershop/static/media/facebook-logo.5d6ab587.png"
+  },
+  {
+    "revision": "86f33cfc1e9c3bc689abc0851fa2f7b5",
+    "url": "/allamericanbarbershop/static/media/insta.86f33cfc.png"
+  }
+]);
